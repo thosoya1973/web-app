f995535fa4ecd643a9a670f8b98b72335b46d290
thosoya1973 <79416770+thosoya1973@users.noreply.github.com>
2021-06-23 20:37:18 +0900

    Update index.js

--- index.js
+++ index.js
@@ -60,6 +60,21 @@ const expenses = [
   },
 ];
 
+app.use(
+ auth({
+  secret: SESSION_SECRET,
+  authRequired: false,
+  auth0Logout: true,
+  baseURL: APP_URL,
+  // 👇 add this 👇
+  authorizationParams: {
+   response_type: "code id_token",
+   audience: "https://expenses-api",
+  },
+  // 👆 add this 👆
+ })
+);
+
 app.get("/", async (req, res) => {
   res.render("home", {
     user: req.oidc && req.oidc.user,
@@ -98,10 +113,30 @@ app.get("/expenses", requiresAuth(), async (req, res, next) => {
     expenses,
   });
 });
-*/
+
 app.get("/expenses", requiresAuth(), async (req, res, next) => {
  try {
   const expenses = await axios.get(`${API_URL}/reports`);
+  res.render("expenses", {
+   user: req.oidc && req.oidc.user,
+   expenses: expenses.data,
+  });
+ } catch (err) {
+  next(err);
+ }
+});
+*/
+app.get("/expenses", requiresAuth(), async (req, res, next) => {
+ try {
+  // 👇 get the token from the request 👇
+  const { token_type, access_token } = req.oidc.accessToken;
+  // 👇 then send it as an authorization header 👇
+  const expenses = await axios.get(`${API_URL}/reports`, {
+   headers: {
+    Authorization: `${token_type} ${access_token}`,
+   },
+  });
+  // 👆 end of changes 👆
   res.render("expenses", {
    user: req.oidc && req.oidc.user,
    expenses: expenses.data,
